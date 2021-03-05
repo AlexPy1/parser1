33f18be2243a19e9898546670855422119ba7a63
Sasha <79185842375@yandex.ru>
2021-03-05 18:59:48 +0300

    Добавил функцию, нужно добавить все чемпионаты и кубки,тк разные блоки

--- parser.py
+++ parser.py
@@ -9,71 +9,72 @@ header ={'user-agent': user}
 link = "https://soccer365.ru/online/"
 response = requests.get(link,headers=header).text
 soup = BeautifulSoup(response, 'lxml')
-block_1 = soup.find('div', id='cmp12')
-
-count=1
-liga=block_1.find_all('span')[count-1].text
-print(liga)
-print()
-try:
-    for i in range(20):
-        time = block_1.find_all('span')[count].text
-        one_team = block_1.find_all('span')[count + 1].text
-        two_team = block_1.find_all('span')[count + 2].text
-        result1 = time + ' ' + one_team + '-' + two_team
-        count += 5
-        print(result1)
-except:
+block_1 = soup.find('div', id='cmp16')
+def today(block):
+    count=1
+    liga=block.find_all('span')[count-1].text
+    print(liga)
     print()
+    try:
+        for i in range(20):
+            time = block.find_all('span')[count].text
+            one_team = block.find_all('span')[count + 1].text
+            two_team = block.find_all('span')[count + 2].text
+            result1 = time + ' ' + one_team + '-' + two_team
+            count += 5
+            print(result1)
+    except:
+        print()
+today(block=block_1)
 
 
-count=1
-block_2 = soup.find('div', id='cmp15')
-liga2=block_2.find_all('span')[count - 1].text
-print(liga2)
-print()
-try:
-    for i in range(20):
-        time = block_2.find_all('span')[count].text
-        one_team = block_2.find_all('span')[count + 1].text
-        two_team = block_2.find_all('span')[count + 2].text
-        result1 = time + ' ' + one_team + '-' + two_team
-        count += 5
-        print(result1)
-except:
-    print()
-
-
-count=1
-block_3 = soup.find('div', id='cmp723')
-liga3=block_3.find_all('span')[count - 1].text
-print(liga3)
-print()
-try:
-    for i in range(20):
-        time = block_3.find_all('span')[count].text
-        one_team = block_3.find_all('span')[count + 1].text
-        two_team = block_3.find_all('span')[count + 2].text
-        result1 = time + ' ' + one_team + '-' + two_team
-        count += 5
-        print(result1)
-except:
-    print()
-
-
-count=1
-block_4 = soup.find('div', id='gm1521614')
-liga4=block_4.find_all('span')[count - 1].text
-print(liga4)
-print()
-try:
-    for i in range(20):
-        time = block_4.find_all('span')[count].text
-        one_team = block_4.find_all('span')[count + 1].text
-        two_team = block_4.find_all('span')[count + 2].text
-        result1 = time + ' ' + one_team + '-' + two_team
-        count += 5
-        print(result1)
-except:
-    print()
+# count=1
+# block_2 = soup.find('div', id='cmp15')
+# liga2=block_2.find_all('span')[count - 1].text
+# print(liga2)
+# print()
+# try:
+#     for i in range(20):
+#         time = block_2.find_all('span')[count].text
+#         one_team = block_2.find_all('span')[count + 1].text
+#         two_team = block_2.find_all('span')[count + 2].text
+#         result1 = time + ' ' + one_team + '-' + two_team
+#         count += 5
+#         print(result1)
+# except:
+#     print()
+#
+#
+# count=1
+# block_3 = soup.find('div', id='cmp723')
+# liga3=block_3.find_all('span')[count - 1].text
+# print(liga3)
+# print()
+# try:
+#     for i in range(20):
+#         time = block_3.find_all('span')[count].text
+#         one_team = block_3.find_all('span')[count + 1].text
+#         two_team = block_3.find_all('span')[count + 2].text
+#         result1 = time + ' ' + one_team + '-' + two_team
+#         count += 5
+#         print(result1)
+# except:
+#     print()
+#
+#
+# count=1
+# block_4 = soup.find('div', id='gm1521614')
+# liga4=block_4.find_all('span')[count - 1].text
+# print(liga4)
+# print()
+# try:
+#     for i in range(20):
+#         time = block_4.find_all('span')[count].text
+#         one_team = block_4.find_all('span')[count + 1].text
+#         two_team = block_4.find_all('span')[count + 2].text
+#         result1 = time + ' ' + one_team + '-' + two_team
+#         count += 5
+#         print(result1)
+# except:
+#     print()
 
